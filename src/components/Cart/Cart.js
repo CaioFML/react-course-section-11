7bfd98f346187a563651e48ccc77daddbcaee1be
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-21 18:39:25 -0300

    147. Working on a More Complex Reducer Logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,8 +7,6 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  console.log(cartCtx)
-
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0
 
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,14 +9,30 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
-
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
-    return { items: updateItems, totalAmount: updatedTotalAmount };
-  } else if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
 
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
 
   return defaultCartState;
